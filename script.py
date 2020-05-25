51faf4b024c1daefdf58bef9f46b6e6e5e82cab5
buiuferro <bruno.ferro@fatec.sp.gov.br>
2020-05-25 12:48:37 -0300

    Add server

--- script.py
+++ script.py
@@ -5,19 +5,18 @@ import matplotlib.pyplot as plt
 import requests
 from io import BytesIO
 
-# Insira a URL da imagem desejada
-url = 'https://i.pinimg.com/564x/dd/6c/cf/dd6ccf8f48adf1fcc7eab0b2a5495661.jpg'
+def criar_filtro(url):
+    
+    # Recebendo a imagem de uma URL
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
 
-# Recebendo a imagem de uma URL
-response = requests.get(url)
-img = Image.open(BytesIO(response.content))
+    # Salvando a imagem com filtro
+    img_filter = img.filter(ImageFilter.CONTOUR)
+    img_filter.save('img.png')
 
-# Salvando a imagem com filtro
-img_filter = img.filter(ImageFilter.CONTOUR)
-img_filter.save('img.png')
+    # Mostrando a imagem com filtro
+    img_filter.show()
 
-# Mostrando a imagem com filtro
-img_filter.show()
-
-# Mostrando a imagem original
-img.show()
+    # Mostrando a imagem original
+# img.show()
