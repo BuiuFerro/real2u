38d82f76057056221af91b5b295476a9d30786ef
buiuferro <bruno.ferro@fatec.sp.gov.br>
2020-05-27 09:01:43 -0300

    React client being served

--- backend/main.py
+++ backend/main.py
@@ -1,44 +1,46 @@
-from flask import flask, request
+import flask
 
-from .script import gerar_img
+from flask import Flask, request, render_template
 
-app = flask(__name__)
+from script import gerar_img
+
+app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def main():
-    return flask.render_template("index.html", token='token')
+    return render_template("index.html", token='token')
     
-    errors = ""
-    if request.method == "POST":
-        url = None
-        try:
-            url = str(request.form["url"])
-        except:
-            errors += "<p>{!r} não é uma URL.</p>\n".format(request.form["url"])
+    # errors = ""
+    # if request.method == "POST":
+    #     url = None
+    #     try:
+    #         url = str(request.form["url"])
+    #     except:
+    #         errors += "<p>{!r} não é uma URL.</p>\n".format(request.form["url"])
        
-        if url is not None:
-            result = gerar_img(url)
-            return '''
-                <html>
-                    <body>
-                        <p>The result is {result}</p>
-                        <p><a href="/">Click aqui para gerar outra imagem</a>
-                    </body>
-                </html>
-            '''.format(result=result)
+    #     if url is not None:
+    #         result = gerar_img(url)
+    #         return '''
+    #             <html>
+    #                 <body>
+    #                     <p>The result is {result}</p>
+    #                     <p><a href="/">Click aqui para gerar outra imagem</a>
+    #                 </body>
+    #             </html>
+    #         '''.format(result=result)
 
-    return '''
-        <html>
-            <body>
-                {errors}
-                <p>Inserir a URL da imagem:</p>
-                <form method="post" action=".">
-                    <p><input name="url" /></p>
-                    <p><input type="submit" value="Gerar filtro!" /></p>
-                </form>
-            </body>
-        </html>
-    '''.format(errors=errors)
+    # return '''
+    #     <html>
+    #         <body>
+    #             {errors}
+    #             <p>Inserir a URL da imagem:</p>
+    #             <form method="post" action=".">
+    #                 <p><input name="url" /></p>
+    #                 <p><input type="submit" value="Gerar filtro!" /></p>
+    #             </form>
+    #         </body>
+    #     </html>
+    # '''.format(errors=errors)
 
 
 if __name__ == "__main__":
